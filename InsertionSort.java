4fa037986af5ee0dfc8bf81cdca2b5209ccea5ac
viditsuri5 <viditsuri5@gmail.com>
2025-01-28 04:31:34 +0000

    changes

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,5 +2,10 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int n = input.length;
+        for (int i = 1; i < n; i++){
+            int key = input[i];
+            int j = i - 1;
+        }
     }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -2,5 +2,17 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+         int n = input.length;
+         for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = 1 + 1; j < n; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+            }
+         }
+        int temp = input[minIndex];
+        input[minIndex] = input[i];
+        input[i] = temp;
+        }
     }
 }
