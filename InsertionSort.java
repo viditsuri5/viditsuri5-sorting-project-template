1644b8bccef140a57d0ad2eb41f502bbf4f474dd
viditsuri5 <viditsuri5@gmail.com>
2025-01-28 05:48:44 +0000

    new changes

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,11 +1,16 @@
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+        //System.out.println("Insertion Sort!!!");
         int n = input.length;
         for (int i = 1; i < n; i++){
             int key = input[i];
             int j = i - 1;
+            while (j >= 0 && input[j] > key) {
+                input[j + 1] = input[j];
+                j--;
+            }
+            input[j + 1] = key;
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,20 +1,22 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        // Task:
-        // 1. Implement missing sorting algorithms.
-        //    - InsertionSort.java
-        //    - SelectionSort.java
-        //
-        //  2. Add a method to class Main to generate a random array.
-        //  3. Provide random input to test suite and ensure your implementations
-        //     of InsertionSort and Selection sort continue to work.
-        //  4. Increase the loop count for TestSuite to get better performance results.
+import java.util.Random;
 
-        int[] testInput = { 1, 2, 3, 1 };
+public class Main {
+    public static void main(String[] args) {
+        // Generate a random array
+        int[] testInput = generateRandomArray(10, 0, 100);
 
         // Increase the loop count to get better results once it works.
-        // TestSuite.run(testInput, 10000);
-        TestSuite.run(testInput, 1);
+        TestSuite.run(testInput, 10000);
+    }
+
+    // Method to generate a random array of integers
+    public static int[] generateRandomArray(int size, int min, int max) {
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(max - min + 1) + min;
+        }
+        return array;
     }
 }
+
